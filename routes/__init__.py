4541638d2bc72a435313fdc76879559bf13342f8
Oluwafunso Oluyole-Balogun <oluwafunsooluyole-balogun@Oluwafunsos-MacBook-Pro.local>
2021-09-04 20:54:44 +0100

    Sets up post route

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,7 +2,8 @@ from flask_restful import Api
 
 # import controllers
 from controllers import (
-    SampleResource, AdminAuthenticationResource
+    SampleResource, AdminAuthenticationResource,
+    PostResource
 )
 
 api = Api()
@@ -10,3 +11,4 @@ api = Api()
 # add routes
 api.add_resource(SampleResource, '/sample', '/sample/')
 api.add_resource(AdminAuthenticationResource, '/api/v1/admin/login', '/api/v1/admin/login/')
+api.add_resource(PostResource, '/api/v1/post', '/api/v1/post/')
