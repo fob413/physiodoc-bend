00e240bb917b1426d44a597f1268d476b279a6c0
Oluwafunso Oluyole-Balogun <oluwafunsooluyole-balogun@Oluwafunsos-MacBook-Pro.local>
2021-09-04 22:48:19 +0100

    Implements get single published post

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,7 +3,8 @@ from flask_restful import Api
 # import controllers
 from controllers import (
     SampleResource, AdminAuthenticationResource,
-    PostResource, PublishPostResource
+    AdminPostResource, PublishPostResource,
+    UnitPostResource
 )
 
 api = Api()
@@ -11,5 +12,6 @@ api = Api()
 # add routes
 api.add_resource(SampleResource, '/sample', '/sample/')
 api.add_resource(AdminAuthenticationResource, '/api/v1/admin/login', '/api/v1/admin/login/')
-api.add_resource(PostResource, '/api/v1/post', '/api/v1/post/')
+api.add_resource(AdminPostResource, '/api/v1/post', '/api/v1/post/')
+api.add_resource(UnitPostResource, '/api/v1/post/<string:post_id>', '/api/v1/post/<string:post_id>/')
 api.add_resource(PublishPostResource, '/api/v1/publish/<string:post_id>', '/api/v1/publish/<string:post_id>/')
